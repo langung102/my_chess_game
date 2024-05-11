e662f244f9b11228b79519a241967d780563c486
langung102 <minhlangwalker@gmail.com>
2024-05-11 21:39:52 +0700

    run ok

---
+++ auto_play.py
@@ -0,0 +1,211 @@
+import random
+
+import pygame
+
+import time
+import chess
+import chess.svg
+from board_evaluator import Evaluation
+
+############################################
+#####SELECT LEVEL (EASY, MEDIUM or HARD)####
+level = "HARD"
+is_white_random = False
+is_black_random = False
+depth_white = 1
+depth_black = 4
+############################################
+
+# Initialize Pygame
+pygame.init()
+
+# Constants
+SCREEN_WIDTH = 512
+SCREEN_HEIGHT = 512
+BOARD_SIZE = 8
+SQUARE_SIZE = SCREEN_WIDTH // BOARD_SIZE
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+HIGHLIGHT_COLOR = (0, 255, 0)
+
+# Font
+font = pygame.font.Font(None, 36)
+
+# Create the screen
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Chess Game")
+
+# Create a chess board
+board = chess.Board()
+
+EVAL = Evaluation(level)
+
+# Function to highlight legal moves for the selected piece
+def highlight_legal_moves(square):
+    legal_moves = board.legal_moves
+    for move in legal_moves:
+        if move.from_square == square:
+            file = chess.square_file(move.to_square)
+            rank = 7 - chess.square_rank(move.to_square)
+            rect = pygame.Rect(file * SQUARE_SIZE, rank * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+            pygame.draw.rect(screen, HIGHLIGHT_COLOR, rect, 4)
+
+# Function to make a random move for the AI side
+def minimax(board, depth, maximizing_player, alpha, beta):
+    if depth == 0 or board.is_game_over():
+        return EVAL.evaluate(board)
+
+    if maximizing_player:
+        max_eval = float('-inf')
+        for move in board.legal_moves:
+            board.push(move)
+            eval = minimax(board, depth - 1, False, alpha, beta)
+            board.pop()
+            max_eval = max(max_eval, eval)
+            alpha = max(alpha, max_eval)
+            if beta <= alpha:
+                break
+        return max_eval
+    else:
+        min_eval = float('inf')
+        for move in board.legal_moves:
+            board.push(move)
+            eval = minimax(board, depth - 1, True, alpha, beta)
+            board.pop()
+            min_eval = min(min_eval, eval)
+            beta = min(beta, min_eval)
+            if beta <= alpha:
+                break
+        return min_eval
+
+def make_minimax_move(board, depth, is_white):
+    best_move = None
+    if is_white:
+        best_eval = float('-inf')
+        for move in board.legal_moves:
+            if chess.square_rank(move.to_square) == 7 and board.piece_at(move.from_square).piece_type == chess.PAWN:
+                move.promotion = chess.QUEEN
+                print("promotion!")
+            board.push(move)
+            eval = minimax(board, depth - 1, False, -10000, 10000)
+            board.pop()
+            if eval > best_eval:
+                best_eval = eval
+                best_move = move    
+    else:
+        best_move = None
+        best_eval = float('inf')
+        for move in board.legal_moves:
+            if chess.square_rank(move.to_square) == 7 and board.piece_at(move.from_square).piece_type == chess.PAWN:
+                move.promotion = chess.QUEEN
+                print("promotion!")
+            board.push(move)
+            eval = minimax(board, depth - 1, True, -10000, 10000)
+            board.pop()
+            if eval < best_eval:
+                best_eval = eval
+                best_move = move
+    board.push(best_move)
+    return best_move
+
+def make_random_move(board):
+    legal_moves = list(board.legal_moves)
+    random_move = random.choice(legal_moves)
+    board.push(random_move)
+    return random_move
+
+# Main loop
+running = True
+selected_square = None
+
+while running and not board.is_game_over():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    if (board.turn == chess.WHITE):
+        if is_white_random:
+            make_random_move(board)
+        else:
+            make_minimax_move(board, depth_white, board.turn == chess.WHITE)
+    else:
+        if is_black_random:
+            make_random_move(board)
+        else:
+            make_minimax_move(board, depth_black, board.turn == chess.WHITE)
+
+    # Draw the chessboard
+    for file in range(BOARD_SIZE):
+        for rank in range(BOARD_SIZE):
+            color = BLACK if (file + rank) % 2 == 0 else WHITE
+            rect = pygame.Rect(
+                file * SQUARE_SIZE, (7 - rank) * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE
+            )
+            pygame.draw.rect(screen, color, rect)
+
+    # Draw the chess pieces
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+        if piece is not None:
+            piece_img = pygame.image.load(
+                f"pieces/{piece.symbol()}.png"
+            ).convert_alpha()
+            # Scale the image
+            piece_img = pygame.transform.scale(piece_img, (SQUARE_SIZE, SQUARE_SIZE))
+            screen.blit(
+                piece_img,
+                (
+                    chess.square_file(square) * SQUARE_SIZE,
+                    (7 - chess.square_rank(square)) * SQUARE_SIZE,
+                ),
+            )
+    # Highlight selected square
+    if selected_square is not None:
+        highlight_rect = pygame.Rect(
+            chess.square_file(selected_square) * SQUARE_SIZE,
+            (7 - chess.square_rank(selected_square)) * SQUARE_SIZE,
+            SQUARE_SIZE,
+            SQUARE_SIZE,
+        )
+        pygame.draw.rect(screen, HIGHLIGHT_COLOR, highlight_rect, 4)
+
+        #Draw possible mobe
+        highlight_legal_moves(selected_square)
+    # Update the display
+    pygame.display.flip()
+    # Check if the game is over
+    if board.is_game_over():
+        print(board.result())
+        if board.outcome().winner == None:
+            text = font.render(f"{board.outcome().termination}", True, (0, 255, 0))
+        else:
+            # Determine the winner
+            (winner, color) = (
+                ("Black", (255, 0, 0))
+                if board.turn == chess.WHITE
+                else ("White", (0, 255, 0))
+            )
+            # Create a text surface
+            text = font.render(f"Game Over: {winner} wins!", True, color)
+
+        # Get the size of the text surface
+        text_rect = text.get_rect()
+
+        # Center the text surface
+        text_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
+        # Draw the text surface on the screen
+        screen.blit(text, text_rect)
+
+        pygame.display.flip()
+
+        running = False
+
+waiting = True
+while waiting:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            waiting = False
+# Quit Pygame
+pygame.quit()
--- board_evaluator.py
+++ board_evaluator.py
@@ -1,47 +1,65 @@
 import chess
 from board_pieces_tables import *
 
-def evaluate(board):
-    """
-    Given a particular board, evaluates it and gives it a score.
-    A higher score indicates it is better for white.
-    A lower score indicates it is better for black.
+class Evaluation:
+    def __init__(self, level):
+        if (level == "EASY"):
+            self.PAWN_TABLE = PAWN_TABLE
+            self.KNIGHTS_TABLE = KNIGHTS_TABLE
+            self.BISHOPS_TABLE = BISHOPS_TABLE
+            self.ROOKS_TABLE = ROOKS_TABLE
+            self.QUEENS_TABLE = QUEENS_TABLE
+            self.KINGS_TABLE = KINGS_TABLE  
+        elif (level == "MEDIUM"):
+            return
+        elif (level == "HARD"):
+            self.PAWN_TABLE = PAWN_TABLE_HARD
+            self.KNIGHTS_TABLE = KNIGHTS_TABLE_HARD
+            self.BISHOPS_TABLE = BISHOPS_TABLE_HARD
+            self.ROOKS_TABLE = ROOKS_TABLE_HARD
+            self.QUEENS_TABLE = QUEENS_TABLE_HARD
+            self.KINGS_TABLE = KINGS_TABLE_HARD
 
-    Args:
-        board (chess.Board): A chess board.
+    def evaluate(self, board):
+        """
+        Given a particular board, evaluates it and gives it a score.
+        A higher score indicates it is better for white.
+        A lower score indicates it is better for black.
 
-    Returns:
-        int: A score indicating the state of the board (higher is good for white, lower is good for black)
-    """    
+        Args:
+            board (chess.Board): A chess board.
 
-    boardvalue = 0
-    
-    wp = len(board.pieces(chess.PAWN, chess.WHITE))
-    bp = len(board.pieces(chess.PAWN, chess.BLACK))
-    wn = len(board.pieces(chess.KNIGHT, chess.WHITE))
-    bn = len(board.pieces(chess.KNIGHT, chess.BLACK))
-    wb = len(board.pieces(chess.BISHOP, chess.WHITE))
-    bb = len(board.pieces(chess.BISHOP, chess.BLACK))
-    wr = len(board.pieces(chess.ROOK, chess.WHITE))
-    br = len(board.pieces(chess.ROOK, chess.BLACK))
-    wq = len(board.pieces(chess.QUEEN, chess.WHITE))
-    bq = len(board.pieces(chess.QUEEN, chess.BLACK))
-    
-    material = 100 * (wp - bp) + 300 * (wn - bn) + 300 * (wb - bb) + 500 * (wr - br) + 900 * (wq - bq)
-    
-    pawn_sum = sum([PAWN_TABLE_HARD[i] for i in board.pieces(chess.PAWN, chess.WHITE)])
-    pawn_sum = pawn_sum + sum([-PAWN_TABLE_HARD[chess.square_mirror(i)] for i in board.pieces(chess.PAWN, chess.BLACK)])
-    knight_sum = sum([KNIGHTS_TABLE_HARD[i] for i in board.pieces(chess.KNIGHT, chess.WHITE)])
-    knight_sum = knight_sum + sum([-KNIGHTS_TABLE_HARD[chess.square_mirror(i)] for i in board.pieces(chess.KNIGHT, chess.BLACK)])
-    bishop_sum = sum([BISHOPS_TABLE_HARD[i] for i in board.pieces(chess.BISHOP, chess.WHITE)])
-    bishop_sum = bishop_sum + sum([-BISHOPS_TABLE_HARD[chess.square_mirror(i)] for i in board.pieces(chess.BISHOP, chess.BLACK)])
-    rook_sum = sum([ROOKS_TABLE_HARD[i] for i in board.pieces(chess.ROOK, chess.WHITE)]) 
-    rook_sum = rook_sum + sum([-ROOKS_TABLE_HARD[chess.square_mirror(i)] for i in board.pieces(chess.ROOK, chess.BLACK)])
-    queens_sum = sum([QUEENS_TABLE_HARD[i] for i in board.pieces(chess.QUEEN, chess.WHITE)]) 
-    queens_sum = queens_sum + sum([-QUEENS_TABLE_HARD[chess.square_mirror(i)] for i in board.pieces(chess.QUEEN, chess.BLACK)])
-    kings_sum = sum([KINGS_TABLE_HARD[i] for i in board.pieces(chess.KING, chess.WHITE)]) 
-    kings_sum = kings_sum + sum([-KINGS_TABLE_HARD[chess.square_mirror(i)] for i in board.pieces(chess.KING, chess.BLACK)])
-    
-    boardvalue = material + pawn_sum + knight_sum + bishop_sum + rook_sum + queens_sum + kings_sum
-    
-    return boardvalue
+        Returns:
+            int: A score indicating the state of the board (higher is good for white, lower is good for black)
+        """    
+
+        boardvalue = 0
+        
+        wp = len(board.pieces(chess.PAWN, chess.WHITE))
+        bp = len(board.pieces(chess.PAWN, chess.BLACK))
+        wn = len(board.pieces(chess.KNIGHT, chess.WHITE))
+        bn = len(board.pieces(chess.KNIGHT, chess.BLACK))
+        wb = len(board.pieces(chess.BISHOP, chess.WHITE))
+        bb = len(board.pieces(chess.BISHOP, chess.BLACK))
+        wr = len(board.pieces(chess.ROOK, chess.WHITE))
+        br = len(board.pieces(chess.ROOK, chess.BLACK))
+        wq = len(board.pieces(chess.QUEEN, chess.WHITE))
+        bq = len(board.pieces(chess.QUEEN, chess.BLACK))
+
+        material = 100 * (wp - bp) + 300 * (wn - bn) + 300 * (wb - bb) + 500 * (wr - br) + 900 * (wq - bq)
+        pawn_sum = sum([self.PAWN_TABLE[i] for i in board.pieces(chess.PAWN, chess.WHITE)])
+        pawn_sum = pawn_sum + sum([-self.PAWN_TABLE[chess.square_mirror(i)] for i in board.pieces(chess.PAWN, chess.BLACK)])
+        knight_sum = sum([self.KNIGHTS_TABLE[i] for i in board.pieces(chess.KNIGHT, chess.WHITE)])
+        knight_sum = knight_sum + sum([-self.KNIGHTS_TABLE[chess.square_mirror(i)] for i in board.pieces(chess.KNIGHT, chess.BLACK)])
+        bishop_sum = sum([self.BISHOPS_TABLE[i] for i in board.pieces(chess.BISHOP, chess.WHITE)])
+        bishop_sum = bishop_sum + sum([-self.BISHOPS_TABLE[chess.square_mirror(i)] for i in board.pieces(chess.BISHOP, chess.BLACK)])
+        rook_sum = sum([self.ROOKS_TABLE[i] for i in board.pieces(chess.ROOK, chess.WHITE)]) 
+        rook_sum = rook_sum + sum([-self.ROOKS_TABLE[chess.square_mirror(i)] for i in board.pieces(chess.ROOK, chess.BLACK)])
+        queens_sum = sum([self.QUEENS_TABLE[i] for i in board.pieces(chess.QUEEN, chess.WHITE)]) 
+        queens_sum = queens_sum + sum([-self.QUEENS_TABLE[chess.square_mirror(i)] for i in board.pieces(chess.QUEEN, chess.BLACK)])
+        kings_sum = sum([self.KINGS_TABLE[i] for i in board.pieces(chess.KING, chess.WHITE)]) 
+        kings_sum = kings_sum + sum([-self.KINGS_TABLE[chess.square_mirror(i)] for i in board.pieces(chess.KING, chess.BLACK)])
+        
+        boardvalue = material + pawn_sum + knight_sum + bishop_sum + rook_sum + queens_sum + kings_sum
+        
+        return boardvalue
